49a0d24bb8ef203104915a357d3ae4c2bfbbd42a
zaburo <zaburo90@gmail.som>
2016-08-30 12:06:26 +0800

    Created by Coding Ground Client

---
+++ PythonTutorial/mysun.py
@@ -0,0 +1,18 @@
+def mysum(numbers):
+    "Take one parameter, an iteral"
+    count = 0
+    for item in nubmers:
+        count+=item
+    return count
+def add_one(x =None)
+if x is none:
+    x=[]
+x.append(1)
+def mysum(items):
+items = list(item)
+output = type(list(items)[0])()
+for one_item in items:
+    output +=one_item
+return output
+
+mysum(raw_input('Enter your numbers: '))
